ae7a3dc501a1e3c0a3a800e4fe88c80ba06d4921
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2024-08-29 15:20:14 +0200

    Update M_Design_RCCMRx.py

--- M_Design_RCCMRx.py
+++ M_Design_RCCMRx.py
@@ -1505,6 +1505,9 @@ def page_RCCMRx() :
     st.write(df_bilan)    
 
     """
+    # On récupère les données des pièces assemblées sous forme d'un tableau DataFrame
+    df_assembly_part_data = st.session_state.test_data 
+    
     # On crée le rapport pdf
     pdf_buffer = create_rapport_pdf_rccmrx()
     st.success("PDF exporté avec succès")

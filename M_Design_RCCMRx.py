03452f1f61dc33584fa11308ab9e368098e2e013
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2024-08-30 11:39:55 +0200

    Update M_Design_RCCMRx.py

--- M_Design_RCCMRx.py
+++ M_Design_RCCMRx.py
@@ -1515,7 +1515,7 @@ def page_RCCMRx() :
     # On crée le rapport pdf
     pdf_buffer = create_rapport_pdf_rccmrx(type_boulonnerie, df_bolt_geom_data_full, df_Bolt_Material_Data, B_acier_aust, df_assembly_part_data, Study_Case, Lambda, 
                               ft, fv, F0, F0_selection, adherence_selection, selection1, selection2, d, h, Le, SyminP_T, SyminB_T, L, e, T_Results_Ansys_Bilan,
-                              critere_selection, L_Result_Boulon_i)
+                              critere_selection, L_marge_full)
     st.success("PDF exporté avec succès")
     
     # Proposer le téléchargement

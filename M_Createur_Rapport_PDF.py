93e7832a2a62f5b4c8dc94d1a5509c4b871c00d7
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2024-09-10 14:08:03 +0200

    Update M_Createur_Rapport_PDF.py

--- M_Createur_Rapport_PDF.py
+++ M_Createur_Rapport_PDF.py
@@ -947,7 +947,7 @@ def create_rapport_pdf_rccmrx(bolt_type, df_bolt_geom_data_full, df_Bolt_Materia
         elements.append(legend)
         elements.append(Spacer(1, 12))  # Ajouter un espace après le texte
     
-    
+    marge_min = 10.0
     text = Paragraph("**Le dimensionnement des liaisons boulonnées étudiées est validé avec une marge minimale de **" + str(marge_min) + " *%*", normal_style)
     elements.append(text)                              
     

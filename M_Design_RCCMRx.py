cb30e29296b4ad9867b458862d1ca2defed84545
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2024-09-11 16:30:46 +0200

    Update M_Design_RCCMRx.py

--- M_Design_RCCMRx.py
+++ M_Design_RCCMRx.py
@@ -1557,9 +1557,7 @@ def page_RCCMRx() :
     file_name = st.text_input("Nom du fichier PDF", placeholder="Rapport.pdf")
     
     if ".pdf" not in file_name :
-        st.write(file_name)
         file_name = file_name + ".pdf"
-        st.write(file_name)
         
     st.download_button(
       label="Télécharger le rapport PDF",

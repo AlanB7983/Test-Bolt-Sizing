919c5d8197d48ff97382ca63674b2478d3567e40
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2024-09-11 10:46:22 +0200

    Update M_Createur_Rapport_PDF.py

--- M_Createur_Rapport_PDF.py
+++ M_Createur_Rapport_PDF.py
@@ -32,7 +32,7 @@ def header_footer(canvas, doc):
     canvas.setFont('Times-Roman', 10)
     canvas.drawString(7 * inch, 10.5 * inch, full_date)
 
-    # Ajouter le logo en haut à gauche (remplacez 'logo.png' par le chemin de votre image)
+    # Ajouter le logo en haut à gauche 
     logo_path = "Pictures/logo.png"  # Le chemin vers votre logo
     canvas.drawImage(logo_path, 0.5 * inch, 10.5 * inch, width=1 * inch, height=1 * inch, preserveAspectRatio=True)
     

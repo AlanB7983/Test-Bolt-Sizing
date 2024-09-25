861719a23d452dafcaa5dead03b5363902041c97
AlanB7983 <162340797+AlanB7983@users.noreply.github.com>
2024-09-25 14:38:02 +0200

    Update M_Design_EUROCODE.py

--- M_Design_EUROCODE.py
+++ M_Design_EUROCODE.py
@@ -1367,7 +1367,7 @@ def page_EUROCODE() :
     L_cat = [check_cat_A, check_cat_B, check_cat_C, check_cat_D, check_cat_E, check_combine]
     
     # On crée le rapport pdf
-    pdf_buffer = create_pdf_eurocode(type_boulonnerie, d, classe, tete_fraisee_check, resine_check, df_bolt_geom_data, df_bolt_geom_data, simple_recouvrement_check, plan_cisaillement, df_torseur_full, L_cat, tp , Lj)
+    pdf_buffer = create_pdf_eurocode(type_boulonnerie, d, classe, tete_fraisee_check, resine_check, df_bolt_geom_data, df_bolt_geom_data, simple_recouvrement_check, plan_cisaillement, df_torseur_full, L_cat, tp , Lj, Result_Cat_A, Result_Cat_B, Result_Cat_C, Result_Cat_D, Result_Cat_E, Result_Cat_Combine, marge_min_A, marge_min_B, marge_min_C, marge_min_D, marge_min_E, marge_min_combine)
     
     
     # Proposer le téléchargement

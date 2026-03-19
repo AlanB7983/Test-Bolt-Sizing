708647225f53812a1f04503167adbfc68972b54f
Endurance Core Lab <162340797+AlanB7983@users.noreply.github.com>
2026-03-19 14:43:34 +0100

    Update M_Design_EUROCODE.py

--- M_Design_EUROCODE.py
+++ M_Design_EUROCODE.py
@@ -1245,7 +1245,8 @@ def page_EUROCODE() :
         nom_boulon = torseur_effort_full_final[i][0]
         position = torseur_effort_full_final[i][1]
         FtEd = float(torseur_effort_full_final[i][2])
-        FtEd_without_preload = (float(torseur_effort_full_final[i][2]) - F0)/Lambda # Nécessaire pour le calcul de la résistance au glissement
+        if F0_selection == "non" :
+            FtEd_without_preload = (float(torseur_effort_full_final[i][2]) - F0)/Lambda # Nécessaire pour le calcul de la résistance au glissement
         FvEd = float(torseur_effort_full_final[i][3])
         # st.write("FtEd = " + str(FtEd))
         # st.write("FvEd = " + str(FvEd))
